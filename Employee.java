29733facf1219c88d3f266633e0c5ea88719853a
sanjeevini2107 <dotragavissd@gmail.com>
2025-05-07 15:37:04 +0530

    Initial commit on master branch

--- Employee.java
+++ Employee.java
@@ -2,22 +2,27 @@ public class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name,int id ,double salary){
-        this.name=name;
-        this.id=id;
-        this.salary=salary;
-    }
-    public void diaplayInfo(){
-        System.out.println("Employee ID:"+id);
-        System.out.println("E,ployee Name"+name);
-        System.out.println("E,ployee Name"+name);
+    String lastname;
 
-        
+    // Constructor updated to accept four parameters
+    public Employee(String name, int id, double salary, String lastname) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.lastname = lastname;
     }
+
+    // Fixed method name typo and corrected the print statement
+    public void displayInfo() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + salary);
+        System.out.println("Employee LastName: " + lastname);
+    }
+
     public static void main(String[] args) {
-        Employee emp1= new Employee("XYZ",101,50000.0);
-        emp1.diaplayInfo();
+        // Now passing all four arguments: id, name, salary, lastname
+        Employee emp1 = new Employee("xyz", 101, 50000.0, "abc");
+        emp1.displayInfo(); // Calling the displayInfo method
     }
 }
-    
-
